2a3a2049eafb40ca32387ddf71c0ce6014c08491
mingl2000 <mingl2000@gmail.com>
2025-09-19 20:27:49 -0700

    add YAHOO_API

--- DataAPI/yahooAPI.py
+++ DataAPI/yahooAPI.py
@@ -55,17 +55,40 @@ class YAHOO_API(CCommonStockApi):
         ]  # 每一列字段
         self.time_column_idx = self.columns.index(DATA_FIELD.FIELD_TIME)
         super(YAHOO_API, self).__init__(code, k_type, begin_date, end_date, autype)
-
+    def get_interval(self):
+        interval_map = {
+            KL_TYPE.K_1M: "1m",
+            KL_TYPE.K_5M: "5m",
+            KL_TYPE.K_15M: "15m",
+            KL_TYPE.K_30M: "30m",
+            KL_TYPE.K_60M: "60m",
+            KL_TYPE.K_DAY: "1d",
+            KL_TYPE.K_WEEK: "1wk",
+            KL_TYPE.K_MON: "1mo",
+            KL_TYPE.K_QUARTER: "3mo",
+        }
+        if self.k_type not in interval_map:
+            raise CChanException(ErrCode.ERR_UNSUPPORTED, f"yahoo api not support {self.k_type}")
+        return interval_map[self.k_type]
     def get_kl_data(self):
         #df=yf.download(self.code, start=self.begin_date, end=self.end_date, interval=self.k_type.name[2:].lower(), progress=False)
-        print(f"download {self.code} from {self.begin_date} to {self.end_date} by {self.k_type.name[2:].lower()}")
-        df=yf.download(self.code, start=self.begin_date, end=self.end_date, interval='1d', progress=False)
+        print(f"download {self.code} from {self.begin_date} to {self.end_date} by {self.get_interval()}")
+        interval=self.get_interval()
+        if interval.endswith("1m"):
+            df=yf.download(self.code, period='7d', interval=interval, progress=False)
+        elif interval.endswith("60m"):
+            df=yf.download(self.code, period='720d', interval=interval, progress=False)
+        elif interval.endswith("m"):
+            df=yf.download(self.code, period='60d', interval=interval, progress=False)
+        else:
+            df=yf.download(self.code, start=self.begin_date, end=self.end_date, interval=interval, progress=False)
         df=df.droplevel(1,axis=1)
         df=df[['Open','High','Low','Close']]
         df=df.reset_index()
+        timeformat='%Y-%m-%d %H:%M:%S' if interval.endswith("m") else '%Y-%m-%d'
         for idx, row in df.iterrows():
             data = [
-                row.values[0].strftime('%Y-%m-%d'),
+                row.values[0].strftime(timeformat),
                 row.values[1],
                 max(row.values[1:5]),
                 min(row.values[1:5]),
